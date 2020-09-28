8dadf001cec75dadd58bb7d178e59da29cdb6557
baba_s <shotababa1206@gmail.com>
2020-09-28 19:44:22 +0900

    first commit

---
+++ Editor/AllSceneSkyboxMaterialRemover.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Kogane.Internal
+{
+	internal static class AllSceneSkyboxMaterialRemover
+	{
+		private const string MENU_ITEM_ROOT = "Edit/UniAllSceneSkyboxMaterialRemover/";
+
+		private static readonly EditorDialog m_editorDialog = new EditorDialog( "UniAllSceneSkyboxMaterialRemover" );
+
+		[MenuItem( MENU_ITEM_ROOT + "すべてのシーンの Skybox Material を null にする" )]
+		private static void ExecuteAll()
+		{
+			if ( !m_editorDialog.OpenYesNo( "すべてのシーンの Skybox Material を null にしますか？" ) ) return;
+
+			SceneProcessor.ProcessAllScenes( OnProcess );
+
+			m_editorDialog.OpenOk( "すべてのシーンの Skybox Material を null にしました" );
+		}
+
+		[MenuItem( MENU_ITEM_ROOT + "Project Settings で設定されているシーンの Skybox Material を null にする" )]
+		private static void ExecuteBy()
+		{
+			if ( !m_editorDialog.OpenYesNo( "Project Settings で設定されているシーンの Skybox Material を null にしますか？" ) ) return;
+
+			var settings = AllSceneSkyboxMaterialRemoverSettings.Load();
+
+			SceneProcessor.ProcessAllScenes
+			(
+				scenePathFilter: scenePath => settings.ScenePathFilter.Any( x => scenePath.StartsWith( x ) ),
+				onProcess: OnProcess
+			);
+
+			m_editorDialog.OpenOk( "Project Settings で設定されているシーンの Skybox Material を null にしました" );
+		}
+
+		private static SceneProcessResult OnProcess( Scene scene )
+		{
+			if ( RenderSettings.skybox == null ) return SceneProcessResult.NOT_CHANGE;
+			RenderSettings.skybox = null;
+			return SceneProcessResult.CHANGE;
+		}
+	}
+}
---
+++ Editor/AllSceneSkyboxMaterialRemoverSettings.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Kogane.Internal
+{
+	/// <summary>
+	/// 設定を管理するクラス
+	/// </summary>
+	internal sealed class AllSceneSkyboxMaterialRemoverSettings : ScriptableObject
+	{
+		//================================================================================
+		// 定数
+		//================================================================================
+		private const string PATH = "ProjectSettings/UniAllSceneSkyboxMaterialRemoverSettings.json";
+
+		//================================================================================
+		// 変数(SerializeField)
+		//================================================================================
+		[SerializeField] private string[] m_scenePathFilter = new string[0];
+
+		//================================================================================
+		// 変数(static)
+		//================================================================================
+		private static AllSceneSkyboxMaterialRemoverSettings m_instance;
+
+		//================================================================================
+		// プロパティ
+		//================================================================================
+		internal IReadOnlyList<string> ScenePathFilter => m_scenePathFilter;
+
+		//================================================================================
+		// 関数(static)
+		//================================================================================
+		/// <summary>
+		/// 設定を ProjectSettings フォルダから読み込みます
+		/// </summary>
+		internal static AllSceneSkyboxMaterialRemoverSettings Load()
+		{
+			return m_instance = ScriptableObjectToJsonFileConverter.Load( PATH, m_instance );
+		}
+
+		/// <summary>
+		/// 設定を ProjectSettings フォルダに保存します
+		/// </summary>
+		internal static void Save()
+		{
+			ScriptableObjectToJsonFileConverter.Save( PATH, m_instance );
+		}
+	}
+}
